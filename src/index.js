3c0c4a8354d33cc1055283b4996ffee6a471599c
Aleksander <onopriyenkoalex@gmail.com>
2022-05-27 19:47:38 +0300

    init: init project with CRA

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import './assets/style/style.css';
+import { App } from './components';
+
+/* eslint-disable react/jsx-filename-extension */
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
